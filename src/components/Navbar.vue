04fad0c437d641e6a8b0045eecbf2bf07769d729
Alessandro-Rega <regaale47@gmail.com>
2021-12-13 17:34:22 +0100

    insert navbar

---
+++ src/components/Navbar.vue
@@ -0,0 +1,77 @@
+<template>
+  <div>
+    <header>
+      <div class="container">
+        <div class="cont-img">
+          <img src="../assets/dc-logo.png" alt="logo dc">
+        </div>
+        <ul>
+          <li>characters</li>
+          <li class="selected">comics</li>
+          <li>movies</li>
+          <li>tv</li>
+          <li>games</li>
+          <li>collectibles</li>
+          <li>videos</li>
+          <li>fans</li>
+          <li>news</li>
+          <li>shop</li>
+        </ul>
+      </div>
+    </header>
+  </div>
+</template>
+
+<script>
+export default {
+    name: 'Navbar'
+}
+</script>
+
+<style scoped>
+
+header{
+  height: 130px;
+  background-color: #fff;
+}
+
+.container{
+  width: 70%;
+  height: 130px;
+  margin: 0 auto;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+}
+
+.cont-img{
+  width: min-content;
+}
+
+
+ul li{
+  display: inline-block;
+  text-transform: uppercase;
+  margin: 0 10px;
+  font-weight: bold;
+  font-size: 15px;
+  cursor: pointer;
+  color: rgb(83, 83, 83);
+}
+
+.selected{
+  color: #0880f8;
+  position: relative;
+}
+
+.selected::after{
+  content: "";
+  position: absolute;
+  bottom: -56px;
+  left: 0;
+  width: 100%;
+  height: 10px;
+  background-color: #0880f8;
+}
+
+</style>
