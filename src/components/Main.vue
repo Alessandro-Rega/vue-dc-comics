5cf0bbee0f5184da4051d73ff9d57ff1a2e406f9
Alessandro-Rega <regaale47@gmail.com>
2021-12-13 17:34:33 +0100

    insert main

---
+++ src/components/Main.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>
+      <main>
+          <span>→ Content goes here ←</span>
+      </main>
+  </div>
+</template>
+
+<script>
+export default {
+    name: 'Main'
+}
+</script>
+
+<style scoped>
+
+div{
+    background-color: #212121;
+}
+
+main{
+    width: 70%;
+    height: 180px;
+    margin: 0 auto;
+    background-color: #212121;
+    display: flex;
+    align-items: center;
+}
+
+span{
+    color: white;
+    font-weight: 800;
+    font-size: 30px;
+}
+
+</style>
