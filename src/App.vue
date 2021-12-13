ab8669b91193d11cce9ca7fd26476c98b8946482
Alessandro-Rega <regaale47@gmail.com>
2021-12-13 18:43:18 +0100

    finalizzazione app.vue

--- src/App.vue
+++ src/App.vue
@@ -2,18 +2,27 @@
   <div id="app">
     <Navbar/>
     <Main/>
+    <SectionMain/>
+    <FooterTop/>
+    <FooterBottom/>
   </div>
 </template>
 
 <script>
 import Navbar from './components/Navbar.vue'
 import Main from './components/Main.vue'
+import SectionMain from './components/SectionMain.vue'
+import FooterTop from './components/FooterTop.vue'
+import FooterBottom from './components/FooterBottom.vue'
 
 export default {
   name: 'App',
   components: {
     Navbar,
     Main,
+    SectionMain,
+    FooterTop,
+    FooterBottom,
   }
 }
 </script>
@@ -29,5 +38,7 @@ export default {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
+  background-color: #1f1f1f;
+  padding-top: 20px;
 }
 </style>
